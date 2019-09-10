56ec35f41013d84cea54ccd4354b45051509d11b
jychoi <smaugdeath@gmail.com>
2019-09-10 13:40:33 +0900

    spring 프로젝트 (스프링 컨테이너 생성)

---
+++ src/test/java/kr/or/ddit/test/board/dao/BoardDao.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.test.board.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BoardDao implements IBoardDao{
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardDao.class);
+	
+	/**
+	* Method : getBoardList
+	* 작성자 : PC-24
+	* 변경이력 :
+	* Method 설명 : 게시판 리스트를 조회한다
+	*/
+	@Override
+	public void getBoardList() {
+		logger.debug("BoardDao.getBoardList()");
+	}
+
+		
+}
---
+++ src/test/java/kr/or/ddit/test/board/service/IBoardService.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.test.board.service;
+
+import kr.or.ddit.test.board.dao.IBoardDao;
+
+public interface IBoardService {
+	
+	/**
+	* Method : getBoardList
+	* 작성자 : PC-24
+	* 변경이력 :
+	* Method 설명 : 게시판 리스트를 조회한다
+	*/
+	void getBoardList();
+	
+	void setBoardDao(IBoardDao boardDao);
+	
+	void setBoardNm(String boardNm);
+}
